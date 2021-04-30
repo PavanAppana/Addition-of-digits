9e21af66f5beb2cd235a3d20340e043f2eec9582
PavanAppana <78023654+PavanAppana@users.noreply.github.com>
2021-04-30 17:35:26 +0530

    Add files via upload

---
+++ Decision.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c,d,sum;
+printf("Enter four integers : ");
+scanf("%d%d%d%d",&a,&b,&c,&d);
+sum = a+b+c+d;
+printf("Sum of %d, %d, %d and %d : %d\n",a,b,c,d,sum);
+}
